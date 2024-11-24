adb680d2bee6488a7a672750bb96d6454365c34d
hussein <86411714+wearelikwid@users.noreply.github.com>
2024-11-24 11:22:54 +0200

    Update firebase-config.js

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -11,5 +11,23 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
+const analytics = firebase.analytics();
 const db = firebase.firestore();
 const auth = firebase.auth();
+
+// Enable persistence
+db.enablePersistence()
+  .catch((err) => {
+    if (err.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled in one tab at a time
+      console.log('Multiple tabs open, persistence can only be enabled in one tab at a time');
+    } else if (err.code == 'unimplemented') {
+      // The current browser doesn't support persistence
+      console.log('The current browser doesn\'t support persistence');
+    }
+  });
+
+// Export the Firebase services for use in other files
+window.db = db;
+window.auth = auth;
+window.app = app;
